8f7d5b645075c92aa289a8852971b8a885e72c66
MichaelYaps <michaelakichi@gmail.com>
2022-12-29 04:05:26 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let words = prompt("Enter several words (separated by commas)");
+let tab = words.split(",");
+
+// Get max words
+let nbStars = 0;
+for (const item of tab) {
+    if (nbStars<item.length) {
+        nbStars = item.length;
+    }
+}
+
+let starsStr = "**";
+for (let index = 0; index < nbStars; index++) {
+    starsStr += "*";
+}
+
+
+console.log(starsStr);
+for (const item of tab) {
+    // add spaces
+    let space = "";
+    if (item.length<nbStars) {
+        let nb = nbStars-item.length;
+        for (let index = 0; index < nb; index++) {
+            space += " ";
+        }
+    }
+    console.log(`*${item+space}*`);
+}
+console.log(starsStr);
